7150870ff7b10b1a5c3358897eb20be1e04a2314
Jame Inw <thejamezas@hotmail.com>
2022-05-10 19:06:07 +0000

    Build LEMP on Docker

---
+++ html/index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+	<title>Resume/CV - Phanuphong Phumphothong</title>
+	
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
+	<link rel="shortcut icon" href="dev.png"> 
+	
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+	
+	<!-- FontAwesome JS-->
+	<script defer src="assets/fontawesome/js/all.min.js"></script>
+	
+	<!-- Theme CSS -->  
+	<link id="theme-style" rel="stylesheet" href="assets/css/devresume.css">
+
+</head> 
+
+<body>
+	
+	<div class="main-wrapper">
+		<div class="container px-3 px-lg-5">
+			<article class="resume-wrapper mx-auto theme-bg-light p-5 mb-5 my-5 shadow-lg">
+				
+				<div class="resume-header">
+					<div class="row align-items-center">
+						<div class="resume-title col-12 col-md-6 col-lg-8 col-xl-9">
+							<h2 class="resume-name mb-0 text-uppercase">Phanuphong Phumphothong</h2>
+							<div class="resume-tagline mb-3 mb-md-0">System Engineer, VOIP Developer</div>
+						</div><!--//resume-title-->
+						<div class="resume-contact col-12 col-md-6 col-lg-4 col-xl-3">
+							<ul class="list-unstyled mb-0">
+								<li class="mb-2"><i class="fas fa-phone-square fa-fw fa-lg me-2 "></i><a class="resume-link" href="tel:#">086 878 9186</a></li>
+								<li class="mb-2"><i class="fas fa-envelope-square fa-fw fa-lg me-2"></i><a class="resume-link" href="mailto:#">phanuphong.p@spinsoft.co.th</a></li>
+								<li class="mb-0"><i class="fas fa-map-marker-alt fa-fw fa-lg me-2"></i>Pathum Thani</li>
+							</ul>
+						</div><!--//resume-contact-->
+					</div><!--//row-->
+					
+				</div><!--//resume-header-->
+				<hr>
+				<div class="resume-intro py-3">
+					<div class="row align-items-center">
+						<div class="col-12 col-md-3 col-xl-2 text-center">
+						    <img class="resume-profile-image mb-3 mb-md-0 me-md-5  ms-md-0 rounded mx-auto" src="assets/images/profile.jpg" alt="image">
+						</div>
+						
+						<div class="col text-start">
+							<p class="mb-0">Hello, my nickname is James, currently working at Spinsoft Company, my position is System Engineer. configuration Server and then developed in the IPPBX system. Related systems are Asterisk, Kamailio, docker, rtpengine, ELK stack. Can work as a team well. Learn quickly and be able to withstand hard work. I am constantly improving myself and I am always learning new things.</p>
+						</div><!--//col-->
+					</div>
+				</div><!--//resume-intro-->
+				<hr>
+				<div class="resume-body">
+					<div class="row">
+						<div class="resume-main col-12 col-lg-8 col-xl-9   pe-0   pe-lg-5">
+							<section class="work-section py-3">
+								<h3 class="text-uppercase resume-section-heading mb-4">Work Experiences</h3>
+								<div class="item mb-3">
+									<div class="item-heading row align-items-center mb-2">
+										<h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">System Engineer</h4>
+										<div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-start text-md-end">Spinsoft | 2021 - Present</div>
+										
+									</div>
+									<div class="item-content">
+										<p>Develop an IP PBX system to improve service efficiency and solve problems that may arise in the future. Working under the National Science and Technology Development Agency (NSTDA) located at Thailand Science Park (TSP) and the system being serviced is</p>
+										<!-- <ul class="resume-list">
+											<li>Emergency medical services (EMS)</li>
+											<li>Thai Telecommunication Relay Service (TTRS)</li>
+										</ul> -->
+									</div>
+								</div><!--//item-->
+							</section><!--//work-section-->
+
+							
+							<section class="project-section py-3">
+								<h3 class="text-uppercase resume-section-heading mb-4">Projects</h3>
+								<div class="item mb-3">
+									<div class="item-heading row align-items-center mb-2">
+										<h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Emergency medical services (EMS)</h4>
+										<div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-start text-md-end">Closed source</div>
+										
+									</div>
+									<div class="item-content">
+										<p>Take care of the IPPBX system for Emergency medical services (EMS) in all 76 provinces and also help to improve the existing system better.</p>
+										
+										
+									</div>
+								</div><!--//item-->
+								<div class="item">
+									<div class="item-heading row align-items-center mb-2">
+										<h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Thai Telecommunication Relay Service (TTRS)</h4>
+										<div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-start text-md-end">Closed source</div>
+										
+									</div>
+									<div class="item-content">
+										<p>Take care of IPPBX system for Thai Telecommunication Relay Service (TTRS) and store user service statistics. It is a system that enables the hearing impaired and the speech impaired to communicate with the general public through basic telecommunication equally and comprehensively.</p>
+										
+									</div>
+								</div><!--//item-->
+							</section><!--//project-section-->
+							<section class="project-section py-3">
+								<h3 class="text-uppercase resume-section-heading mb-4">CERTIFICATE</h3>
+								<div class="item mb-3">
+									<div class="item-heading row align-items-center mb-2">
+										<h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">CCNA R&S: Routing and Switching Essentials</h4>
+										<div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-start text-md-end">Issued by Cisco</div>
+										
+									</div>
+									<div class="item-content">
+										<p>This course focuses on switching technologies and router operations that support small-to-medium business networks, including wireless local area networks (WLAN) and security concepts. In this second course in a 3-course CCNA series you’ll perform basic network configuration and troubleshooting, identify and mitigate LAN security threats, and configure and secure a basic WLAN. Recommended preparation: CCNA: Introduction to Networks or having equivalent knowledge. After completing all three CCNA courses, you are ready to take the CCNA Certification. </p>
+										
+										
+									</div>
+								</div><!--//item-->
+								<div class="item">
+									<div class="item-heading row align-items-center mb-2">
+										<h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">CCNAv7: Switching, Routing, and Wireless Essentials</h4>
+										<div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-start text-md-end">Issued by Cisco</div>
+										
+									</div>
+									<div class="item-content">
+										<p>The second course in the CCNA curriculum focuses on switching technologies and router operations that support small-to-medium business networks and includes wireless local area networks (WLAN) and security concepts. Students learn key switching and routing concepts. They can perform basic network configuration and troubleshooting, identify and mitigate LAN security threats, and configure and secure a basic WLAN.</p>
+										
+									</div>
+								</div><!--//item-->
+								<div class="item">
+									<div class="item-heading row align-items-center mb-2">
+										<h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">CyberOps Associate</h4>
+										<div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-start text-md-end">Issued by Cisco</div>
+										
+									</div>
+									<div class="item-content">
+										<p>Cisco Certified CyberOps Associate certification program validates the day-today, tactical knowledge and skills that Security Operations Center (SOC) teams need to detect and respond to cybersecurity threats. The CyberOps Associate exam and training cover knowledge and skills related to security concepts, security monitoring, hostbased analysis, network intrusion analysis, and security policies and procedures.</p>
+										
+									</div>
+								</div><!--//item-->
+								<div class="item">
+									<div class="item-heading row align-items-center mb-2">
+										<h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Cloud Essentials</h4>
+										<div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-start text-md-end">Issued by CompTIA</div>
+										
+									</div>
+									<div class="item-content">
+										<p>CompTIA Cloud Essentials+ is the only internationally recognized, vendor-neutral certification utilizing key business principles and fundamental cloud concepts that validate data-driven cloud recommendations. It stands alone in this field by demonstrating that all necessary staff members – not just the IT specialists – understand how to increase efficiency, manage costs, and reduce security risks for organizations whenever tasked with making current cloud technology decisions.</p>
+										
+									</div>
+								</div><!--//item-->
+								<div class="item">
+									<div class="item-heading row align-items-center mb-2">
+										<h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Cybersecurity Essentials</h4>
+										<div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-start text-md-end">Issued by Cisco</div>
+										
+									</div>
+									<div class="item-content">
+										<p>The holder of this credential has a foundational knowledge of cybersecurity, including the basics of network security, DevSecOps (Development, Security, Operations), encryption and cybersecurity laws. The holder has been introduced to tactics and techniques used by Black Hats, and the principles of Confidentiality, Integrity and Availability (CIA) used by White Hats to defend networks. This credential also certifies that the holder has practiced critical thinking and problem solving skills.</p>
+										
+									</div>
+								</div><!--//item-->
+							</section><!--//project-section-->	
+						</div><!--//resume-main-->
+						<aside class="resume-aside col-12 col-lg-4 col-xl-3 px-lg-4 pb-lg-4">
+							<section class="skills-section py-3">
+								<h3 class="text-uppercase resume-section-heading mb-4">Skills</h3>
+								<div class="item">
+									<h4 class="item-title">Technical</h4>
+									<ul class="list-unstyled resume-skills-list">
+										<li class="mb-2">Golang/PHP/Python</li>
+										<li class="mb-2">Shell script/MySQL/Docker<li>
+										<li class="mb-2">Linux/Java/JavaScript</li>
+										<li class="mb-2">Cisco Routing & Switching</li>
+										<li class="mb-2">Adobe Illustrator</li>
+										<li class="mb-2">Object-oriented design</li>
+										<li class="mb-2">Design and implement database structures</li>
+										</ul>
+									</div><!--//item-->
+									<div class="item">
+										<h4 class="item-title">Professional</h4>
+										<ul class="list-unstyled resume-skills-list">
+											<li class="mb-2">Effective communication</li>
+											<li class="mb-2">Team player<li>
+												<li class="mb-2">Strong problem solver</li>
+												<li>Good time management</li>
+											</ul>
+										</div><!--//item-->
+									</section><!--//skills-section-->
+									<section class="education-section py-3">
+										<h3 class="text-uppercase resume-section-heading mb-4">Education</h3>
+										<ul class="list-unstyled resume-education-list">
+											<li class="mb-3">
+												<div class="resume-degree font-weight-bold">Department of Computer Engineering</div>
+												<div class="resume-degree-org text-muted">Rajamangala University of Technology Thanyaburi</div>
+												<div class="resume-degree-time text-muted">2018 - 2021</div>
+											</li>
+											<li class="mb-3">
+												<div class="resume-degree font-weight-bold">Computer network</div>
+												<div class="resume-degree-org text-muted">Ubon Ratchathani Technical College</div>
+												<div class="resume-degree-time text-muted">2017 - 2018</div>
+											</li>
+											<li class="mb-3">
+												<div class="resume-degree font-weight-bold">Computer Technology</div>
+												<div class="resume-degree-org text-muted">Ubon Ratchathani Technical College</div>
+												<div class="resume-degree-time text-muted">2015 - 2017</div>
+											</li>
+										</ul>
+									</section><!--//education-section-->
+									<section class="education-section py-3">
+										<h3 class="text-uppercase resume-section-heading mb-4">Awards</h3>
+										<ul class="list-unstyled resume-awards-list">
+											<li class="mb-3">
+												<div class="font-weight-bold">Web-based Management System For Contest Junior Inventor 2017</div>
+												<div class="text-muted">Award winning Gold medal level</div>
+											</li>
+											<li>
+												<div class="font-weight-bold">Computer Network Management Skills 2017</div>
+												<div class="text-muted">Award winning Gold medal level</div>
+											</li>
+										</ul>
+									</section><!--//education-section-->
+									<section class="skills-section py-3">
+										<h3 class="text-uppercase resume-section-heading mb-4">Languages</h3>
+										<ul class="list-unstyled resume-lang-list">
+											<li class="mb-2">Thai <span class="text-muted">(Native)</span></li>
+											<li>English <span class="text-muted">(Intermediate)</span></li>
+										</ul>
+									</section><!--//certificates-section-->
+									<section class="skills-section py-3">
+										<h3 class="text-uppercase resume-section-heading mb-4">Interests</h3>
+										<ul class="list-unstyled resume-interests-list mb-0">
+											<li class="mb-2">Games</li>
+											<li class="mb-2">Movie</li>
+											<li class="mb-2">Photography</li>
+											<li>Travelling</li>
+										</ul>
+									</section><!--//certificates-section-->
+									
+								</aside><!--//resume-aside-->
+							</div><!--//row-->
+						</div><!--//resume-body-->
+						<hr>
+						<div class="resume-footer text-center">
+							<ul class="resume-social-list list-inline mx-auto mb-0 d-inline-block text-muted">
+								<li class="list-inline-item mb-lg-0 me-3"><a class="resume-link" href="https://github.com/JameInw"><i class="fab fa-github-square fa-2x me-2" data-fa-transform="down-4"></i><span class="d-none d-lg-inline-block text-muted">github.com/JameInw</span></a></li>
+								<li class="list-inline-item mb-lg-0 me-3"><a class="resume-link" href="https://www.linkedin.com/in/phanuphong-phumphothong-9a8705138/"><i class="fab fa-linkedin fa-2x me-2" data-fa-transform="down-4"></i><span class="d-none d-lg-inline-block text-muted">linkedin.com/in/phanuphong-phumphothong-9a8705138</span></a></li>
+							</ul>
+						</div><!--//resume-footer-->
+					</article>
+					
+				</div><!--//container-->
+				
+				<footer class="footer text-center py-4">
+					<!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+				</footer>
+				
+			</div><!--//main-wrapper-->
+			
+
+</body>
+</html> 
+
